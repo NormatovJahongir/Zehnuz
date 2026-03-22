b522eeac18f3a4137ebee4042316585978e2eea2
NormatovJahongir <normatovjahongir8@gmail.com>
2026-03-22 20:14:07 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,29 @@ const nextConfig = {
       { protocol: 'http', hostname: 'localhost' },
     ],
   },
+  // CSP xatoligini tuzatish uchun headers qo'shamiz
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: `
+              default-src 'self';
+              script-src 'self' 'unsafe-eval' 'unsafe-inline' https://unpkg.com;
+              style-src 'self' 'unsafe-inline' https://unpkg.com;
+              img-src 'self' blob: data: https://unpkg.com https://*.openstreetmap.org;
+              font-src 'self' data:;
+              connect-src 'self' https://*.openstreetmap.org;
+              frame-ancestors 'none';
+              upgrade-insecure-requests;
+            `.replace(/\s{2,}/g, ' ').trim(),
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
